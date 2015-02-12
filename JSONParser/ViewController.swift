2402dba18944774a51f5585ed41265e21fc22390
Victor Chan <victor.z.chan@gmail.com>
2015-02-12 16:18:52 +1100

    Initial Commit

---
+++ JSONParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParser
+//
+//  Created by Victor Chan on 12/2/15.
+//  Copyright (c) 2015 Spark Plug Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
